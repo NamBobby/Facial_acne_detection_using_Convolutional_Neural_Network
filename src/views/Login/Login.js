23cc970eb31abbc5ae3dbf06bad3d30b4dc2f441
Nam Bobby <lethpnam27@gmail.com>
2023-06-08 15:42:23 +0700

    aaaaa

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import LoginStyle from './styles/LoginStyle';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
-import { deleteAllData , initDatabase, addUser, getUserByEmailAndPassword, getUserByEmail} from '../../../database';
+import { deleteAllData, initDatabase, addUser, getUserByEmailAndPassword, getUserByEmail } from '../../../database';
 
 const Login = () => {
 
@@ -13,9 +14,11 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   useEffect(() => {
-    //deleteAllData(); 
+    //deleteAllData(); // Chạy khi muốn reset dữ liệu mặc định
     initDatabase(); // Khởi tạo cơ sở dữ liệu
   }, []);
 
@@ -26,6 +29,10 @@ const Login = () => {
   const handleSignIn = async () => {
     if (email === '' || password === '') {
       Alert.alert('Thông báo', 'Vui lòng điền đầy đủ thông tin.');
+      return;
+    }
+    if (!isValidEmail(email)) {
+      Alert.alert('Thông báo', 'Email không hợp lệ.');
       return;
     }
     try {
@@ -45,6 +52,14 @@ const Login = () => {
       Alert.alert('Thông báo', 'Vui lòng điền đầy đủ thông tin.');
       return;
     }
+    if (!isValidEmail(email)) {
+      Alert.alert('Thông báo', 'Email không hợp lệ.');
+      return;
+    }
+    if (password.length < 8) {
+      Alert.alert('Thông báo', 'Mật khẩu phải có ít nhất 8 ký tự.');
+      return;
+    }
     if (password !== confirmPassword) {
       Alert.alert('Thông báo', 'Mật khẩu và xác nhận mật khẩu không khớp.');
       return;
@@ -61,7 +76,19 @@ const Login = () => {
       console.log('Lỗi khi thêm dữ liệu:', error);
     }
   };
-  
+
+  const isValidEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <View style={LoginStyle.container}>
       <View style={LoginStyle.topinfo}>
@@ -73,21 +100,33 @@ const Login = () => {
             <Text style={LoginStyle.texttitle}>Please fill your detail to access your account.</Text>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Email</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="Email"
-                onChangeText={(text) => setEmail(text)}
-              
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Email"
+                  onChangeText={(text) => setEmail(text)}
+                />
+              </View>
             </View>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Password</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={(text) => setPassword(text)}
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Password"
+                  secureTextEntry={!showPassword}
+                  onChangeText={(text) => setPassword(text)}
+                />
+                <TouchableOpacity style={LoginStyle.showPasswordButton} onPress={toggleShowPassword}>
+                  <Text style={LoginStyle.text}>
+                    {showPassword ?
+                      (<FontAwesomeIcon icon="eye" size={25} color="#EDD8E9" />
+                      ) : (
+                        <FontAwesomeIcon icon="eye-slash" size={25} color="#EDD8E9" />
+                      )}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
             <TouchableOpacity style={LoginStyle.buttonIn} onPress={handleSignIn}>
               <Text style={LoginStyle.buttonText}>Sign In</Text>
@@ -98,37 +137,63 @@ const Login = () => {
             <Text style={LoginStyle.texttitle}>Please fill your detail to access your account.</Text>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Username</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="Username"
-                onChangeText={(text) => setName(text)}
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Username"
+                  onChangeText={(text) => setName(text)}
+                />
+              </View>
             </View>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Email</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="Email"
-                onChangeText={(text) => setEmail(text)}
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Email"
+                  onChangeText={(text) => setEmail(text)}
+                />
+              </View>
             </View>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Password</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={(text) => setPassword(text)}
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Password"
+                  secureTextEntry={!showPassword}
+                  onChangeText={(text) => setPassword(text)}
+                />
+                <TouchableOpacity style={LoginStyle.showPasswordButton} onPress={toggleShowPassword}>
+                  <Text style={LoginStyle.text}>
+                    {showPassword ?
+                      (<FontAwesomeIcon icon="eye" size={25} color="#EDD8E9" />
+                      ) : (
+                        <FontAwesomeIcon icon="eye-slash" size={25} color="#EDD8E9" />
+                      )}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={LoginStyle.inputText}>
               <Text style={LoginStyle.text}>Password Confirm</Text>
-              <TextInput
-                style={LoginStyle.input}
-                placeholder="PasswordConfirm"
-                secureTextEntry={true}
-                onChangeText={(text) => setConfirmPassword(text)}
-              />
+              <View style={LoginStyle.inputkhung}>
+                <TextInput
+                  style={LoginStyle.input}
+                  placeholder="Password Confirm"
+                  secureTextEntry={!showConfirmPassword}
+                  onChangeText={(text) => setConfirmPassword(text)}
+                />
+                <TouchableOpacity style={LoginStyle.showPasswordButton} onPress={toggleShowConfirmPassword}>
+                  <Text style={LoginStyle.text}>
+                    {showConfirmPassword ?
+                      (<FontAwesomeIcon icon="eye" size={25} color="#EDD8E9" />
+                      ) : (
+                        <FontAwesomeIcon icon="eye-slash" size={25} color="#EDD8E9" />
+                      )}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
             <TouchableOpacity style={LoginStyle.buttonUp} onPress={handleSignUp}>
               <Text style={LoginStyle.buttonText}>Sign Up</Text>
@@ -138,12 +203,12 @@ const Login = () => {
         <TouchableOpacity style={LoginStyle.toggleButton} onPress={handleToggle}>
           <Text style={LoginStyle.toggleButtonText}>
             {showSignIn ? (
-            <Text style={LoginStyle.text}>Don't have an account? <Text style={LoginStyle.textsignIn}>Sign Up</Text></Text>
+              <Text style={LoginStyle.text}>Don't have an account? <Text style={LoginStyle.textsignIn}>Sign Up</Text></Text>
             )
-            :
-            (
-            <Text style={LoginStyle.text}>Already have an account? <Text style={LoginStyle.textsignUp}>Sign In</Text></Text>
-            )}
+              :
+              (
+                <Text style={LoginStyle.text}>Already have an account? <Text style={LoginStyle.textsignUp}>Sign In</Text></Text>
+              )}
           </Text>
         </TouchableOpacity>
       </View>
