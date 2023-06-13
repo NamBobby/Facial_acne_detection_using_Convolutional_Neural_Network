200ff6e96b2bd258a151ebbe020af542f6f8d53e
waterfairy244 <71392398+waterfairy244@users.noreply.github.com>
2023-06-13 12:40:43 +0700

    test

--- src/api/api.py
+++ src/api/api.py
@@ -99,4 +99,4 @@ def get_image(filename):
     return send_from_directory(os.path.join(current_dir, 'static', 'images'), filename)
 
 if __name__ == '__main__':
-    app.run(host='192.168.100.5', port=5000)
+    app.run(host='10.238.44.97', port=5000)
